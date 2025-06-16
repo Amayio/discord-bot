0fff45ddf8adfaeb390cc4beedc20ebf0d56e479
Patryk <patrykprzybylek94@gmail.com>
2025-06-17 00:58:04 +0200

    bug fix allowing multiple reactions

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
-import { Client, GatewayIntentBits, Collection, Events } from 'discord.js';
+import {
+	Client,
+	GatewayIntentBits,
+	Collection,
+	Events,
+	Partials,
+} from 'discord.js';
 import 'dotenv/config';
 import { readdirSync } from 'fs';
 import path from 'path';
@@ -7,7 +13,19 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({
+	partials: [
+		Partials.Message,
+		Partials.Channel,
+		Partials.Reaction,
+		Partials.User,
+	],
+	intents: [
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.GuildMessageReactions,
+	],
+});
 client.commands = new Collection();
 
 const commandsPath = path.join(__dirname, 'commands');
@@ -40,6 +58,31 @@ client.on(Events.InteractionCreate, async interaction => {
 			ephemeral: true,
 		});
 	}
+});
+
+const TARGET_CHANNEL_ID = '810471727746121742';
+
+client.on('messageReactionAdd', async (reaction, user) => {
+	if (reaction.partial) {
+		try {
+			await reaction.fetch();
+		} catch (error) {
+			console.error("Can't get reaction informations", error);
+		}
+	}
+
+	if (user.bot) return;
+	if (reaction.message.channel.id !== TARGET_CHANNEL_ID) return;
+
+	const allReactions = reaction.message.reactions.cache;
+
+	allReactions
+		.filter(
+			reply =>
+				reply.users.cache.has(user.id) &&
+				reply.emoji.name !== reaction.emoji.name,
+		)
+		.forEach(reply => reply.users.remove(user.id));
 });
 
 client.login(process.env.TOKEN);
